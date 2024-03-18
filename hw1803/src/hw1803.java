99c8ef64e4242fb18f2c7c7c67de3a70bc8d812d
Alexey Onoprienko <onalex911@gmail.com>
2024-03-19 01:03:00 +0500

    рисуется первая фигура (из 7-ми)

--- hw1803/src/hw1803.java
+++ hw1803/src/hw1803.java
@@ -1,5 +1,27 @@
 public class hw1803 {
     public static void main(String[] args){
-        System.out.println("Hi!");
+        byte colRow = 7;
+        byte colCol = (byte) 15;
+        byte zvStart;
+        byte zvEnd;
+        byte delta = (byte)(colCol - colRow);
+
+        //равнобедренный треугольник
+        for(byte i = 0; i < colRow; i++){
+            zvStart = (byte)(delta - i);//8
+            zvEnd = (byte)(delta + i);//8
+            for (byte j = 0; j < colCol; j++) {
+                 if(j<zvStart)
+                     System.out.print(" ");
+                 else if(j>zvEnd)
+                     continue;
+                 else
+                     System.out.print("*");
+            }
+            System.out.println();
+        }
+        //прямоугольный треугольник
+        //прямоугольный треугольник основанием вверх
+        //нак
     }
 }
