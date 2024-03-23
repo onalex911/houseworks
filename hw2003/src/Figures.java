0de597ce914fdd6756c8abac73ed00875d89c60a
Alexey Onoprienko <onalex911@gmail.com>
2024-03-23 19:18:34 +0500

    Все числовые переменные переведены в тип byte

--- hw2003/src/Figures.java
+++ hw2003/src/Figures.java
@@ -3,65 +3,70 @@ import java.util.Scanner;
 public class Figures {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        byte max = 5;
         System.out.println("Выберите номер фигуры, которую требуется нарисовать:");
         System.out.println(" 1 - прямоугольный треугольник вершиной вверх;");
         System.out.println(" 2 - прямоугольный треугольник вершиной вниз;");
         System.out.println(" 3 - равнобедренный треугольник вершиной вверх;");
         System.out.println(" 4 - равнобедренный треугольник вершиной вниз;");
-        System.out.println(" 5 - ромб.");
+        System.out.println(" 5 - ромб;");
+        System.out.println(" 0 - вывести все фигуры последовательно.");
         byte typeFigure = sc.nextByte();
         boolean filled;
         char filler = '*';//символ "заполнитель" фигуры
         char spacer = ' ';//символ "пустоты"
 
-        if(typeFigure > 0 && typeFigure <=5){
+        if(typeFigure >= 0 && typeFigure <=max){
             System.out.println("Фигура должна быть закрашена?\n 0 (и меньше) - нет\n 1 (и больше) - да:");
             filled = sc.nextByte() > 0;
-            String nech = typeFigure >= 3 ? "нечетное " : "";
-            System.out.print("Введите кол-во строк для высоты фигуры ("+nech+"число от 3 до 99):");
+            System.out.print("Введите кол-во строк для высоты фигуры (нечетное число от 3 до 99):");
             byte height = sc.nextByte();
-            if(height < 3 || height > 99){
+            if(height < 3 || height > 99)
                 System.out.println("ОШИБКА! Введено неверное число строк.");
-            }else{
-                if(typeFigure >=3 && height%2 == 0){
-                    System.out.println("ВНИМАНИЕ! Вы ввели четное число ("+height+"). Оно будет уменьшено на 1-цу.");
-                    height--; //уменьшаем значение на 1, если введено четное число
-                }
-                int i = -1;
-                int end = height - 1; //предпоследняя позиция (для расчета ширины)
-                while(++i < height){
-                    int width = 0; //ширина фигуры
-                    int start = 0; //начальная позиция, с которой начинается левый край фигуры
-                    switch (typeFigure) {
-                        //прямоугольный треугольник вершиной вверх
-                        case 1: width = i;
+            else if(height%2 == 0)
+                System.out.println("ОШИБКА! Введено четное число строк.");
+            else{
+                byte end = (byte)(height - 1); //предпоследняя позиция (для расчета ширины)
+                byte kolFig = typeFigure == 0 ? max : 1;
+                byte k = 0;
+                while(++k <= kolFig){
+                    if(kolFig > 1) typeFigure = k;
+                    byte i = -1;
+                    while(++i < height){
+                        byte width = 0; //ширина фигуры
+                        byte start = 0; //начальная позиция, с которой начинается левый край фигуры
+                        switch (typeFigure) {
+                            //прямоугольный треугольник вершиной вверх
+                            case 1: width = i;
                                 break;
-                        //прямоугольный треугольник вершиной вниз
-                        case 2: width = end - i;
+                            //прямоугольный треугольник вершиной вниз
+                            case 2: width = (byte)(end - i);
                                 break;
-                        //равнобедренный треугольник вершиной вверх
-                        case 3: width = end + i;
-                                start = end - i;
+                            //равнобедренный треугольник вершиной вверх
+                            case 3: width = (byte)(end + i);
+                                start = (byte)(end - i);
                                 break;
-                        //равнобедренный треугольник вершиной вниз
-                        case 4: width = 2*end - i;
+                            //равнобедренный треугольник вершиной вниз
+                            case 4: width = (byte)(2*end - i);
                                 start = i;
                                 break;
-                        //ромб
-                        case 5: int delta = i <= end/2 ? i : end - i;
-                                width = end/2 + delta;
-                                start = end/2 - delta;
+                            //ромб
+                            case 5: byte delta = i <= end/2 ? i : (byte)(end - i);
+                                width = (byte)(end/2 + delta);
+                                start = (byte)(end/2 - delta);
                                 break;
+                        }
+                        //рисуем одну строку
+                        byte j = -1;
+                        while(++j < width){
+                            if (j < start || (!filled && j > start) && ( i > 0 && i < height - 1))
+                                System.out.print(spacer);
+                            else
+                                System.out.print(filler);
+                        }
+                        System.out.println(filler); //любая строка любой фигуры заканчивается "заполнителем"
                     }
-                    //рисуем одну строку
-                    int j = -1;
-                    while(++j < width){
-                        if (j < start || (!filled && j > start) && ( i > 0 && i < height - 1))
-                            System.out.print(spacer);
-                        else
-                            System.out.print(filler);
-                    }
-                    System.out.println(filler); //любая строка любой фигуры заканчивается "заполнителем"
+                    System.out.println();
                 }
             }
         }else
