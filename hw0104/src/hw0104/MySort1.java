efca4dc5231885109ee7de6a2de6a2aef4627f99
Alexey Onoprienko <onalex911@gmail.com>
2024-04-12 10:05:11 +0500

    Сортировка циклического нахождения минимума без исп. методов; с вычислением максимума массива и его учетом

--- hw0104/src/hw0104/MySort1.java
+++ hw0104/src/hw0104/MySort1.java
@@ -1,83 +1,75 @@
 package hw0104;
-
 import java.util.Random;
 
 public class MySort1 {
-//    public static final int[] inArr = new int[10];
-    public static final int[] inArr = new int[15];// = {400,-1,56,-20,-1};
-    public static int[] ind = new int[inArr.length];
-
-    static int getIndMin(){
-        int min = (int) Math.pow(2,32);
-        int indMin = 0;
-        for (int i = 0; i < inArr.length; i++) {
-            if(isInInd(i)) continue;
-            if(inArr[i]<min) {
-                min = inArr[i];
-                indMin = i;
-            }
-        }
-        return indMin;
-    }
-
-    static boolean isInInd(int index){
-        for (int i = 0; i < ind.length; i++)
-            if(ind[i] == index)
-                return true;
-        return false;
-    }
 
     public static void main(String[] args) {
-
-        int[] outArr = new int[inArr.length];
+        int[] inArr = new int[10];              //исходный массив
+        int[] ind = new int[inArr.length];      //массив индексов найденных минимумов
+        int[] outArr = new int[inArr.length];   //заготовка под отсортированный массив
+        int count = 0;
+        //инициализируем массив индексов "невозможными" значениями
+        int incredible = -1;
         for (int i = 0; i < ind.length; i++) {
-            ind[i]= -1; //инициализируем массив индексов "невозможными" значениями
+            ind[i]= incredible;
         }
 
         //заполняем исходный массив случайными значениями
-        int maxVal = 50;
+        int maxVal = 100;//Integer.MAX_VALUE;
         Random rnd = new Random();
-        System.out.print("inArr: ");
+        System.out.println("исходный массив: ");
         for (int i = 0; i < inArr.length; i++) {
             inArr[i] = rnd.nextInt(maxVal) - rnd.nextInt(maxVal);
-            System.out.print(inArr[i]+" ");
+            System.out.printf("%d, ",inArr[i]);
+        }
+        //находим максимальное значение массива и записываем его индекс...
+        int arrMax = inArr[0];
+        for (int i = 1; i < inArr.length; i++) {
+            if (inArr[i] > arrMax) {
+                arrMax = inArr[i];
+                ind[ind.length - 1] = i;
+            }
+            count++;
+        }
+        //...и записываем его в выходной массив в крайнюю позиция
+        outArr[outArr.length-1] = arrMax;
+        //System.out.println("\nMax = "+arrMax);
+
+        //ищем минимумы, циклически проверяя массив и находя минимальное значение
+        for (int i = 0; i < inArr.length-1; i++) {
+            int min = arrMax;
+            int indMin = 0;
+            for (int j = 0; j < inArr.length; j++) {
+                boolean isInInd = false;
+                //проверяем, есть ли уже данная позиция в массиве индексов...
+                for (int k = 0; k < ind.length; k++) {
+                    if(ind[k] == incredible) break;//пропускаем "невозможные" значения
+                    if (ind[k] == j) {
+                        isInInd = true;
+                        break;
+                    }
+                    count++;
+                }
+                if(isInInd) continue; //...если есть - пропускаем итерацию
+                if(inArr[j]<=min) {
+                    min = inArr[j];
+                    indMin = j;
+                }
+                count++;
+            }
+            ind[i] = indMin; //записываем индекс минимального значения
+            outArr[i] = inArr[indMin]; //записываем очередное минимальное значение
+            count++;
         }
 
-        for (int i = 0; i < inArr.length; i++) {
-            ind[i] = getIndMin();
-        }
-        for (int i = 0; i < ind.length; i++) {
-            outArr[i] = inArr[ind[i]];
-        }
-
-        System.out.print("\nind: ");
+        /*System.out.print("\nмассив индексов: ");
         for (int i = 0; i < ind.length; i++) {
             System.out.print(ind[i]+" ");
-        }
-        System.out.println("\noutArr: ");
+        }*/
+        System.out.println("\nотсортированный массив: ");
         for (int i = 0; i < outArr.length; i++) {
             System.out.print(outArr[i]+" ");
         }
-        //System.out.println("\nCount = "+count);
-
-
-        //сортировка последовательным сравнением
-        /*for (int i = 0; i < inArr.length; i++) {
-            for (int j = i+1; j < inArr.length; j++) {
-                if(inArr[j] < inArr[i])
-                    inArr[j] = (inArr[i] + inArr[j]) - (inArr[i] = inArr[j]);
-            }
-        }*/
-        //сортировка пузырьком
-        /*for (int i = 0; i < inArr.length-1; i++) {
-            for (int j = 0; j < inArr.length-i-1; j++) {
-                if(inArr[j] > inArr[j+1])
-                    inArr[j] = (inArr[j+1] + inArr[j]) - (inArr[j+1] = inArr[j]);
-            }
-        }
-        System.out.println();
-        for (int j : inArr) {
-            System.out.printf("%d ", j);
-        }*/
+        System.out.println("\n\ncount = "+count);
     }
 }
