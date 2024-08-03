ef4e92ea11bb9fef8c8d292e7405d3a4ff1d3f52
Alexey Onoprienko <onalex911@gmail.com>
2024-08-03 15:42:53 +0500

    решение ДЗ от 31.07 через executorService

--- hw0107/src/hw3107/Main3107.java
+++ hw0107/src/hw3107/Main3107.java
@@ -2,67 +2,14 @@ package hw3107;
 
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
-class MyThread1 extends Thread{
-    private char[] text;
-    private long count;
-
-    public MyThread1(char[] text) {
-        this.text = text;
-        count = 0;
-    }
-    @Override
-    public void run() {
-//        super.run();
-        System.out.println("Start t1");
-        for (int i = 0; i < text.length; i++) {
-            for (int j = 0; j < Main3107.glas.length; j++) {
-                if(text[i] == Main3107.glas[j]) {
-                    count++;
-                    break;
-                }
-            }
-        }
-
-        System.out.println("End t1");
-    }
-
-    public long getCount() {
-        return count;
-    }
-}class MyThread2 extends Thread{
-    private char[] text;
-    private long count;
-
-    public MyThread2(char[] text) {
-        this.text = text;
-        count = 0;
-    }
-    @Override
-    public void run() {
-//        super.run();
-        System.out.println("Start t2");
-        for (int i = 0; i < text.length; i++) {
-            if(Main3107.isLetter(text[i])){
-                boolean flag = true;
-                for (int j = 0; j < Main3107.glas.length; j++) {
-                    if (text[i] == Main3107.glas[j]) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if(flag)
-                    count++;
-            }
-        }
-        System.out.println("End t2");
-    }
-
-    public long getCount() {
-        return count;
-    }
-}
 
 //Создать программу которая будет считать из файла который содержит N-количество слов ( сказку)
 //1) Количество глассных символов
@@ -74,46 +21,143 @@ class MyThread1 extends Thread{
 //
 //Выполнить данное задание используя потоки
 public class Main3107 {
-    public static char[] glas = {'a','e','i','o','u'};
 
-    public static boolean isNumber(char ch){
-        return ch >= 48 && (int) ch <= 57;
-    }public static boolean isLetter(char ch){
-        return ch >= 65 && (int) ch <= 90 || (int) ch >= 97 && (int) ch <= 122;
-    }
+    public static String glas = "aeiouyAEIOUY";
+    public static String soglas = "bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ";
+    public static String numbers = "1234567890";
+
     public static void main(String[] args) {
 
-        long startTime =  System.nanoTime();
+        Scanner scn1 = new Scanner(System.in);
+        System.out.print("Введите символ, количество которого нужно подсчитать: ");
+        String symbForSearch = scn1.next();
+        Scanner scn2 = new Scanner(System.in);
+        System.out.print("Введите слово, количество которого нужно подсчитать: ");
+        String wordForSearch = scn2.next();
+
+        long startTime = System.nanoTime();
         File file = new File("Main3107/lorem1.txt");
         if (file.exists()) {
             try {
-                FileReader fr = new FileReader(file);
-                //System.out.println(file.length());
-                char[] buffer = new char[(int)file.length()];
-                int countChars = fr.read(buffer);
-                if(countChars > 0) {
-                    Thread MyThread3 = new Thread((buffer)->{
-            System.out.println("Start MyThread3");
 
-            System.out.println("End MyThread3");
-        });
-                    MyThread1 t1 = new MyThread1(buffer);
-                    t1.start();
+                ExecutorService executorService = Executors.newFixedThreadPool(6);
 
-                    MyThread2 t2 = new MyThread2(buffer);
-                    t2.start();
+                //1) Количество глассных символов
+                Runnable task1 = () -> {
+                    try {
+                        FileReader fr = new FileReader(file);
+                        char[] buffer = new char[(int) file.length()];
+                        int countChars = fr.read(buffer);
+                        if (countChars > 0) {
+                            long count = IntStream.range(0, buffer.length)
+                                    .mapToObj(i -> buffer[i])
+                                    .filter(c -> glas.indexOf(c) != -1)
+                                    .count();
+                            System.out.println("1) Гласных: " + count);
+                        }
+                    } catch (IOException ioe) {
+                    }
+                };
+                //2) Количество согласных символов
+                Runnable task2 = () -> {
+                    try {
+                        FileReader fr = new FileReader(file);
+                        char[] buffer = new char[(int) file.length()];
+                        int countChars = fr.read(buffer);
+                        if (countChars > 0) {
+                            long count = IntStream.range(0, buffer.length)
+                                    .mapToObj(i -> buffer[i])
+                                    .filter(c -> soglas.indexOf(c) != -1)
+                                    .count();
+                            System.out.println("2) Согласных: " + count);
+                        }
+                    } catch (IOException ioe) {
+                    }
 
-                    t1.join();
-                    t2.join();
+                };
+                //3) Количество чисел символов
+                Runnable task3 = () -> {
+                    try {
+                        FileReader fr = new FileReader(file);
+                        char[] buffer = new char[(int) file.length()];
+                        int countChars = fr.read(buffer);
+                        if (countChars > 0) {
+                            long count = IntStream.range(0, buffer.length)
+                                    .mapToObj(i -> buffer[i])
+                                    .filter(c -> numbers.indexOf(c) != -1)
+                                    .count();
+                            System.out.println("3) Числовых символов: " + count);
+                        }
+                    } catch (IOException ioe) {
+                    }
+                };
+                //4) Количество символов
+                Runnable task4 = () -> {
+                    try {
+                        FileReader fr = new FileReader(file);
+                        char[] buffer = new char[(int) file.length()];
+                        int countChars = fr.read(buffer);
+                        System.out.println("4) Всего символов: " + countChars);
+                    }catch (IOException ioe){
+                    }
+                };
+                //5) Количество указанного символа
+                char firstChar = symbForSearch.toCharArray()[0];
+                Runnable task5 = () -> {
+                    try {
+                        FileReader fr = new FileReader(file);
+                        char[] buffer = new char[(int) file.length()];
+                        int countChars = fr.read(buffer);
+                        if (countChars > 0) {
+                            long count = IntStream.range(0, buffer.length)
+                                    .mapToObj(i -> buffer[i])
+                                    .filter(c -> (char) c == firstChar)
+                                    .count();
+                            System.out.println("5) Символов \'" + firstChar + "\': " + count);
+                        }
+                    } catch (IOException ioe) {
+                    }
+                };
+                //6) Количество указанного слово
+                Runnable task6 = () -> {
+                    try {
+                        FileReader fr = new FileReader(file);
+                        char[] buffer = new char[(int) file.length()];
+                        int countChars = fr.read(buffer);
+                        if (countChars > 0) {
+                            String word = "";
+                            long count = 0;
+                            for (int i = 0; i < buffer.length; i++) {
+                                if (buffer[i] == ' ' || buffer[i] == '\n' || buffer[i] == '.' || buffer[i] == ',' || buffer[i] == '!' || buffer[i] == '?' || buffer[i] == ':' || buffer[i] == ';' && word.length() > 0) {
+                                    if (word.equalsIgnoreCase(wordForSearch)) {
+                                        count++;
+                                    }
+                                    word = "";
+                                } else {
+                                    word += buffer[i];
+                                }
+                            }
+                            if (word.equalsIgnoreCase(wordForSearch))
+                                count++;
+                            System.out.println("6) Слов \'" + wordForSearch + "\': " + count);
+                        }
+                    } catch (IOException ioe) {
+                    }
+                };
+                executorService.execute(task1);
+                executorService.execute(task2);
+                executorService.execute(task3);
+                executorService.execute(task4);
+                executorService.execute(task5);
+                executorService.execute(task6);
+                executorService.shutdown();
 
-                    //System.out.println("File length = "+file.length()+", countChars = "+countChars);
-                    System.out.println("glas = " + t1.getCount() + ", soglas = " + t2.getCount());
-                }
-            }catch(Exception ex){
+            } catch (IllegalArgumentException ex) {
                 System.out.println(ex.getMessage());
             }
         }
+
         long endTime = System.nanoTime();
-        System.out.println("Total time: "+(endTime-startTime)/1_000_000);
+        System.out.println("Total time: " + (endTime - startTime) / 1_000_000 + " ms");
     }
 }
