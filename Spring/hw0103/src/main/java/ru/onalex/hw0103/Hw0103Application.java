d8352863a1fec1417fa3aa8a6083f1f782f114ce
Alexey Onoprienko <onalex911@gmail.com>
2025-03-02 17:32:45 +0500

    добавлено задание от 01.03 с раелизацией связей one-to-one

---
+++ Spring/hw0103/src/main/java/ru/onalex/hw0103/Hw0103Application.java
@@ -0,0 +1,13 @@
+package ru.onalex.hw0103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw0103Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw0103Application.class, args);
+    }
+
+}
---
+++ Spring/hw0103/src/test/java/ru/onalex/hw0103/Hw0103ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.onalex.hw0103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw0103ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
